02cb1e3817cb564fba84ec8e89f4b38eed898743
Machiko Yasuda <machikoyasuda@gmail.com>
2015-11-09 23:20:52 -0800

    Clean up code

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
   // Function: Formatting pop-up
-  function formatPopup(name,address,city,zip,year,description,url){
+  function formatPopup(name, address, city, zip, year, description, url){
     if (description === "NULL"){
       description = 'No description available';
     }
@@ -8,16 +8,28 @@ $(document).ready(function(){
   }
 
   // Function: calculate color by decade
-  function calculateColorDecade(year) {
-    return year < 1910 ? "#00ffff" :
-    year > 1910 && year < 1920? "#00c8ff" :
-    year > 1920 && year < 1930? "#0096ff" :
-    year > 1930 && year < 1940? "#0064ff" :
-    year > 1940 && year < 1950? "#6432ff" :
-    year > 1950 && year < 1960? "#9600ff" :
-    year > 1960 && year < 1970? "#c800ff" :
-    year > 1970 && year < 1980? "#ff00ff" :
-    "#ff6cd9";
+  function getColorBy(year) {
+    var color;
+    if (year < 1910) {
+      color = "#00ffff";
+    } else if (year > 1910 && year < 1920) {
+      color = "#00c8ff";
+    } else if (year > 1920 && year < 1930) {
+      color = "#0096ff";
+    } else if (year > 1930 && year < 1940) {
+      color = "#0064ff";
+    } else if (year > 1940 && year < 1950) {
+      color = "#6432ff";
+    } else if (year > 1950 && year < 1960) {
+      color = "#9600ff";
+    } else if (year > 1960 && year < 1970) {
+      color = "#c800ff";
+    } else if (year > 1970 && year < 1980) {
+      color = "#ff00ff";
+    } else {
+      color = "#ff6cd9";
+    }
+    return color;
   }
 
   // Create map
@@ -30,28 +42,28 @@ $(document).ready(function(){
 
   // Add each restaurant's popup
   var onEachFeature = function(feature, layer){
-    var name = feature.properties.Name;
-    var address = feature.properties.Address;
-    var city = feature.properties.City;
-    var zip = feature.properties.Zip;
-    var year = feature.properties.Year;
+    var name        = feature.properties.Name;
+    var address     = feature.properties.Address;
+    var city        = feature.properties.City;
+    var zip         = feature.properties.Zip;
+    var year        = feature.properties.Year;
     var description = feature.properties.Descriptio;
-    var url = feature.properties.Link;
-    var popupText = formatPopup(name,address,city,zip,year,description,url);
+    var url         = feature.properties.Link;
+    var popupText   = formatPopup(name, address, city, zip, year, description, url);
     layer.bindPopup(popupText);
   };
 
   // Add data layer
   var geojsonLayer = new L.GeoJSON.AJAX("data/historic_restaurants.geojson",{
-    onEachFeature:onEachFeature,
+    onEachFeature: onEachFeature,
     pointToLayer: function(feature, latlon) {
       return L.circleMarker(latlon,{
-        fillColor:calculateColorDecade(feature.properties.Year),
-        color: '#FFF',
-        weight: 2,
-        opacity: 1,
+        fillColor:   getColorBy(feature.properties.Year),
+        color:       '#FFFFFF',
+        weight:      2,
+        opacity:     1,
         fillOpacity: 0.5,
-        radius: 6
+        radius:      6
       })
     }
   });
@@ -63,11 +75,11 @@ $(document).ready(function(){
     var div = L.DomUtil.create('div', 'info legend'),
         years = [1900,1910,1920,1930,1940,1950,1960,1970,1980],
         labels = [];
-    for (var i = 0; i < years.length; i++) {
-        div.innerHTML +=
-            '<i style="background:' + calculateColorDecade(years[i] + 1) + '"></i> ' +
-            years[i] + (years[i + 1] ? '&ndash;' + years[i + 1] + '<br>' : '+');
-    }
+    years.forEach(function(year, index) {
+      div.innerHTML +=
+          '<i style="background:' + getColorBy(year + 1) + '"></i> ' +
+          year + (years[index + 1] ? '&ndash;' + years[index + 1] + '<br>' : '+');
+    })
     return div;
   };
   legend.addTo(map);
